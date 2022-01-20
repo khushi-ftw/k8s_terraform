486eaa81b0eefc992960f477b619977be4341c56
Farah Khurrum <fakhurru@microsoft.com>
2022-01-20 14:36:38 -0800

    adding subnet

--- variables.tf
+++ variables.tf
@@ -15,3 +15,12 @@ variable "vpc_network_cidr" {
   default = "10.0.0.0/16" 
 }
 
+variable "vpc_pvt_subnet_cidr" {
+  description = "range of cidr for pvt subnet"
+  type = map(string)
+  default = {
+    "a" = "10.0.0.0/21"
+    "b" = "10.0.8.0/21"
+    "c" = "10.0.16.0/21"
+  }
+}
